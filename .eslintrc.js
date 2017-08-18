81e32637a6d50b3134b3f5a823f46f2d7f8951b1
osuga <osuga.shunya@mfkessai.co.jp>
2017-08-18 10:42:16 +0900

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+// http://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+  },
+  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+  extends: 'standard',
+  // required to lint *.vue files
+  plugins: [
+    'html'
+  ],
+  // add your custom rules here
+  'rules': {
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+  }
+}
